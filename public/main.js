5c101970631518c4153326e58e4f7bc7aefc53d3
Tom Anderson <tom@ando.gq>
2021-04-03 00:12:08 +1100

    Basic code to cache the exercises and display them randomly

--- public/main.js
+++ public/main.js
@@ -1,7 +1,35 @@
 let db = firebase.firestore();
 
-db.collection("exercises").get().then(query => {
-    query.forEach(doc => {
-        console.log(doc.data());
-    });
+let exercises = [];
+
+// Listener for new exercise additions
+db.collection("exercises").onSnapshot(snapshot => {
+    exercises = snapshot.docs.map(exercise => exercise.data());
+    console.log("Exercises updated");
+});
+
+// Generates a random exercise from the database
+function random_exercise() {
+    return exercises[Math.floor(Math.random() * exercises.length)];
+}
+
+// Stores elements on the page
+const el = {
+    button: document.getElementById("new_exercise"),
+    output: document.getElementById("output")
+}
+
+// Event listener to add a random exercise and display it
+el.button.addEventListener("click", () => {
+    el.output.innerHTML = "";
+
+    let exercise = random_exercise();
+
+    let header = document.createElement("h3");
+    header.innerText = exercise.name;
+    el.output.appendChild(header);
+
+    let description = document.createElement("p");
+    description.innerText = exercise.description;
+    el.output.appendChild(description);
 });
--- scripts/load_data.js
+++ scripts/load_data.js
@@ -7,10 +7,29 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-["Star Jump", "Push Up"].forEach(async (exercise, i) => {
-    await db.collection("exercises").doc(`${i}`).set({
-        name: exercise
-    });
+const data = {
+    "star_jump": {
+        "name": "Star Jump",
+        "min": 5,
+        "max": 20,
+        "description": "Blah blah blah"
+    },
+    "push_up": {
+        "name": "Push Up",
+        "min": 5,
+        "max": 20,
+        "description": "Stuff yeah wow"
+    },
+    "neck_stretch": {
+        "name": "Neck Stretch",
+        "min": 2,
+        "max": 6,
+        "description": "Do a stretch"
+    }
+}
+
+Object.keys(data).forEach(async (exercise) => {
+    await db.collection("exercises").doc(exercise).set(data[exercise]);
 });
 
 console.log("Exercises added");
