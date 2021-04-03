977fd7670857c2e8ad31b7cb0fe9bc5c8e0fcacb
Fibre P <fttx5525@gmail.com>
2021-04-03 13:08:34 +1100

    ADD LANDING PAGE BASE UI

---
+++ landing-page/script.js
@@ -0,0 +1,15 @@
+// DOM Elements
+const aShowDivHidden = document.querySelector(".a-show-div-hidden");
+const divHidden = document.querySelector(".div-hidden");
+const selectGroup = document.querySelector(".select-group");
+
+// hide GIFS on load i.e. unscrollable until user clicks .a-show-div-hidden
+// document.onload = divHidden.hidden = true;
+
+// display hidden div onclick
+// aShowDivHidden.onclick = showDivHidden();
+// function showDivHidden() {
+//     divHidden.hidden = false;
+// }
+
+// populate selectGroup with registered Group names
