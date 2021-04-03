d4c04665529ec395b5f7555918f4029c213233b5
Tom Anderson <tom@ando.gq>
2021-04-03 14:16:26 +1100

    Simple sign in and sign out

--- public/main.js
+++ public/main.js
@@ -15,7 +15,13 @@ function random_exercise() {
 
 // Stores elements on the page
 const el = {
-    output: document.getElementById("output")
+    output: document.getElementById("output"),
+    account_form: document.getElementById("account"),
+    register_button: document.getElementById("register"),
+    sign_in_button: document.getElementById("sign_in"),
+    sign_out_botton: document.getElementById("sign_out"),
+    signed_in: document.getElementById("signed_in"),
+    signed_out: document.getElementById("signed_out")
 }
 
 if ('serviceWorker' in navigator) {
@@ -42,3 +48,45 @@ if ('serviceWorker' in navigator) {
         }
     });
 }
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        console.log("User signed in");
+        el.signed_in.style.display = "";
+        el.signed_out.style.display = "none";
+    } else {
+        console.log("User signed out");
+        el.signed_in.style.display = "none";
+        el.signed_out.style.display = "";
+    }
+})
+
+el.register_button.addEventListener("click", () => {
+    let email = el.account_form.elements["email"].value;
+    let password = el.account_form.elements["password"].value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
+        console.log("Registered and signed in successfully");
+    }).catch(error => {
+        console.error(error);
+    });
+});
+
+el.sign_in_button.addEventListener("click", () => {
+    let email = el.account_form.elements["email"].value;
+    let password = el.account_form.elements["password"].value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+        console.log("Signed in successfully");
+    }).catch(error => {
+        console.error(error);
+    });
+});
+
+el.sign_out_botton.addEventListener("click", () => {
+    firebase.auth().signOut().then(() => {
+        console.log("Signed out successfully");
+    }).catch(error => {
+        console.error(error);
+    });
+});
